e4f2a3676517901d77cab85a17f27e1e4a40907c
xAzarkan <sofiane.azarkan@helb-prigogine.be>
2022-05-15 18:22:29 +0200

    commit

--- EasyTravel_Sofiane_AZARKAN/app/src/main/java/com/example/easytravel_sofiane_azarkan/CalendarActivity.java
+++ EasyTravel_Sofiane_AZARKAN/app/src/main/java/com/example/easytravel_sofiane_azarkan/CalendarActivity.java
@@ -3,14 +3,21 @@ package com.example.easytravel_sofiane_azarkan;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CalendarActivity extends AppCompatActivity {
 
     CalendarView calendarView;
     TextView myStartDate;
-    String startDate;
+    String startDate = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +34,36 @@ public class CalendarActivity extends AppCompatActivity {
 
                 startDate = day + "/" + (month+1) + "/" + year;
                 myStartDate.setText("Start : " + startDate);
-
             }
         });
     }
 
-    public void saveDate()
+    public void saveDate(View view)
     { //sauvegarder la date dans un fichier txt pour ensuite afficher une notif
+        if(startDate != "")
+        {
+            try
+            {
+                File myDateFile = new File(getFilesDir(), "dateOfVacation.txt");
+                myDateFile.setWritable(true);
+                myDateFile.createNewFile(); // if file already exists will do nothing
+
+                FileWriter myFileWriter = new FileWriter(myDateFile); //j'ouvre le fichier.txt en écriture
+
+                myFileWriter.write(startDate); //écriture dans le fichier
+
+                myFileWriter.close();
+
+                Toast.makeText(this, "The date was successfully saved!", Toast.LENGTH_SHORT).show();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Toast.makeText(this, "Please select a date first", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
